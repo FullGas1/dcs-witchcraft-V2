7afc8fee0c09d0906b845aaa8bedc53541d5491e
FullGas1 <51051389+FullGas1@users.noreply.github.com>
2026-04-05 17:32:36 +0200

    commit

--- tesf.lua
+++ tesf.lua
@@ -1,1 +1,3 @@
-return Group.getByName("para_carrier"):getUnits()[1]:getName()   --:detroy()
+-- select this file + adapte the groupName you want to test + shortcut  Ctrl+LShift+B -> execute the script
+local groupName = "para_carrier" -- adapt this name to your groupName in mission running
+return Group.getByName(groupName):getUnits()[1]:getName()
